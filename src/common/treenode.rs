a113ca9c1a7c7e1d1a6885b4bc31ee404960f40d
shungiku <s.tada@ntt.com>
2024-05-03 08:07:11 -0400

    add 'from' method for TreeNode

--- src/common/treenode.rs
+++ src/common/treenode.rs
@@ -1,3 +1,4 @@
+use std::collections::VecDeque;
 use std::{cell::RefCell, rc::Rc};
 
 // Definition for a binary tree node.
@@ -16,5 +17,75 @@ impl TreeNode {
             left: None,
             right: None,
         }
+    }
+
+    pub fn from(vec: &Vec<Option<i32>>) -> Option<Self> {
+        let root_val_option = vec[0];
+        if root_val_option.is_none() {
+            return None;
+        }
+
+        let root = Rc::new(RefCell::new(TreeNode::new(root_val_option.unwrap())));
+
+        let mut queue: VecDeque<Option<Rc<RefCell<TreeNode>>>> = VecDeque::new();
+        queue.push_back(Some(Rc::clone(&root)));
+
+        for index in 1..vec.len() {
+            let is_right_node = index % 2 == 0;
+
+            let node_val = vec[index];
+            match node_val {
+                Some(val) => {
+                    let new_node = Rc::new(RefCell::new(TreeNode::new(val)));
+                    queue.push_back(Some(Rc::clone(&new_node)));
+                    let parent_node_ref = &mut queue[0];
+
+                    if let Some(parent_node) = parent_node_ref {
+                        if is_right_node {
+                            parent_node.borrow_mut().right = Some(new_node);
+                        } else {
+                            parent_node.borrow_mut().left = Some(new_node);
+                        }
+                    }
+                }
+                None => {
+                    queue.push_back(None);
+                }
+            }
+
+            if is_right_node {
+                queue.pop_front();
+            }
+        }
+
+        return Some(Rc::try_unwrap(root).unwrap().into_inner());
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use super::TreeNode;
+
+    #[test]
+    fn test() {
+        let input = vec![Some(3), Some(9), Some(20), None, None, Some(15), Some(7)];
+
+        let root = TreeNode::from(&input);
+
+        let node_9 = root.as_ref().unwrap().left.as_ref().unwrap().borrow();
+        let node_20 = root.as_ref().unwrap().right.as_ref().unwrap().borrow();
+        let node_15 = node_20.left.as_ref().unwrap().borrow();
+        let node_7 = node_20.right.as_ref().unwrap().borrow();
+
+        // assert
+        assert_eq!(root.as_ref().unwrap().val, 3);
+        assert_eq!(node_9.val, 9);
+        assert!(node_9.left.is_none());
+        assert!(node_9.right.is_none());
+        assert_eq!(node_20.val, 20);
+        assert_eq!(node_15.val, 15);
+        assert_eq!(node_7.val, 7);
+        assert!(node_7.left.is_none());
+        assert!(node_7.right.is_none());
     }
 }
