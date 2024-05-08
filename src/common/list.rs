e2550ae88c400c8d08f0ed5679a95f6a3eaa6486
stada526 <s.tada@ntt.com>
2024-05-07 23:06:36 -0400

    add ListNode 'from' method

--- src/common/list.rs
+++ src/common/list.rs
@@ -9,4 +9,36 @@ impl ListNode {
     pub fn new(val: i32) -> Self {
         ListNode { next: None, val }
     }
+
+    pub fn from(vec: &Vec<i32>) -> Option<Box<Self>> {
+        let mut next_node = None;
+        for val in vec.iter().rev() {
+            let mut new_node = ListNode::new(*val);
+            new_node.next = next_node.take();
+            next_node = Some(Box::new(new_node));
+        }
+        return match next_node {
+            Some(node) => Some(Box::new(*node)),
+            None => None,
+        };
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn test() {
+        let input = vec![1, 2, 3, 4];
+
+        let mut head = ListNode::from(&input);
+
+        for expected in [1, 2, 3, 4] {
+            assert_eq!(head.as_ref().unwrap().val, expected);
+            head = head.unwrap().next;
+        }
+
+        assert!(head.is_none());
+    }
 }
